506f72b51928204e6749772a10701447b63977cd
cvbach <caovanbach18@gmail.com>
2026-06-01 22:21:30 +0700

    add k8s

--- app/app.py
+++ app/app.py
@@ -1,12 +1,21 @@
 from flask import Flask
+import os
+
 app = Flask(__name__)
+
 @app.route("/")
 def home():
-    return "Welcome to Azure AKS DevOps Project v1.0"
+    return {
+        "application": "Azure DevOps Platform",
+        "environment": os.getenv("ENVIRONMENT", "dev"),
+        "version": os.getenv("APP_VERSION", "1.0.0")
+    }
+
 @app.route("/health")
 def health():
     return {
         "status": "healthy"
     }
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
