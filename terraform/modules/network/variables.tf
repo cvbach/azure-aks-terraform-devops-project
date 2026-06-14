cca35f27f4200ee735fc24c36bf846f285b9ba6a
cvbach <caovanbach18@gmail.com>
2026-06-14 17:46:59 +0700

    test v1

--- terraform/modules/network/variables.tf
+++ terraform/modules/network/variables.tf
@@ -10,6 +10,16 @@ variable "vnet_name" {
 #NSG
 locals {
   nsg_common_rules = [
-   
+   {
+     name     = "Allow-80"
+     priority = 1001
+     direction = "Inbound"
+     access = "Allow"
+     protocol = "Tcp"
+     source_port_range = "*"
+     destination_port_range = "80"
+     source_address_prefix = "*"
+     destination_address_prefix = "*"
+   }
   ]
 }
