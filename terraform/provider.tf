61f317de49158a59bd8ed505116a79d4b70bd86b
cvbach <caovanbach18@gmail.com>
2026-06-20 10:40:52 +0700

    change terrafrom to remote state, create k8s.yml - config k8s

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -12,3 +12,12 @@ provider "azurerm" {
   subscription_id = "6f5f4050-50e7-47b8-a834-65da592adec8"
   features {}
 }
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg-tfstate"
+    storage_account_name = "stv2tfstate"
+    container_name       = "tfstate"
+    key                  = "aks-devops-project.tfstate"
+  }
+}
