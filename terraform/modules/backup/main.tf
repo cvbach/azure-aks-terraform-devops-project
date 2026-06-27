0db815053b669847974823d6536583804c2b47ab
cvbach <caovanbach18@gmail.com>
2026-06-27 20:13:50 +0700

    add backup policy

--- terraform/modules/backup/main.tf
+++ terraform/modules/backup/main.tf
@@ -7,3 +7,21 @@ resource "azurerm_data_protection_backup_vault" "backup" {
 
   redundancy = "LocallyRedundant"
 }
+
+resource "azurerm_data_protection_backup_policy_kubernetes_cluster" "policy" {
+
+  name                = "daily-backup-policy"
+  vault_name          = azurerm_data_protection_backup_vault.backup.name   
+  resource_group_name = var.resource_group_name
+
+  backup_repeating_time_intervals = [
+    "R/2026-01-01T02:00:00+00:00/P1D"
+  ]
+
+  default_retention_rule {
+    life_cycle {
+      duration        = "P7D"
+      data_store_type = "VaultStore"
+    }
+  }
+}
